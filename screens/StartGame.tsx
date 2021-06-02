0aef7e5eb35c95bbed6cf89dfde96cf6019a74f9
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-06-02 13:55:43 -0300

    creating a custom component

--- screens/StartGame.tsx
+++ screens/StartGame.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import Card from "../components/Card";
 
 const StartGame = (props: any) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new game</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -26,17 +27,7 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
+   
   },
   title: {
     fontSize: 20,
